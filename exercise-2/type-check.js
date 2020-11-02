9de2c11ab22f9977272d2dc91f5d5e5ad08bcf31
nath026 <nathalie.ton026@gmail.com>
2020-11-02 22:55:05 +0100

    type_check_v1 modified

--- exercise-2/type-check.js
+++ exercise-2/type-check.js
@@ -16,7 +16,7 @@ function type_check_v1(value, typeToCheck) {
   }
     return value !== null && !Array.isArray(value);
 }
-console.log("1"+type_check_v1("null", "null"));
+/*console.log("1"+type_check_v1("null", "null"));
 console.log("2"+type_check_v1("null", "object"));
 console.log("3"+type_check_v1(null, "object")); //false
 console.log("4"+type_check_v1(null, "null"));
@@ -27,7 +27,7 @@ console.log("7bis"+type_check_v1(function f(){},"object"));//false
 var undeclaredVar;
 console.log("8"+type_check_v1(undeclaredVar,"undefined"));
 console.log("9"+type_check_v1(undeclaredVar,"object")); //false
-
+*/
 function type_check_v2(){
 
 }
